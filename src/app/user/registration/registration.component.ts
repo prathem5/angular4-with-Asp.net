59ae1b9fce444f195a52432761b5c793cf4f388b
prathem5 <44468347+prathem5@users.noreply.github.com>
2020-07-30 00:59:16 +0530

    Add files via upload

---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,41 @@
+
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(public service: UserService) { }
+
+  ngOnInit() {
+    this.service.formModel.reset();
+  }
+  onSubmit(){
+    this.service.register().subscribe(
+      (res:any) =>{
+        if(res.succeeded){
+          this.service.formModel.reset();
+        }else{
+          res.errors.forEach(element => {
+            switch (element.code) {
+              case 'DuplicateUserName':
+                
+                break;
+            
+              default:
+                break;
+            }
+          });
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+}
